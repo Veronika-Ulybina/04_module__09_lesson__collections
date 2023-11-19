5cb565fd4ba8a28523a69c3380d25e778bab8ae8
Veronika Ulybina <veronica.ulybina@yandex.ru>
2023-11-19 23:21:28 +0500

    added file .eslint

--- js/cart.js
+++ js/cart.js
@@ -1,34 +1,34 @@
 'use strict';
 
 const cart = {
-	items: [],
-	totalPrice: 0,
-	count: 0,
-	add(name, price, amount = 1) {
-		this.items.push({name, price, amount});
+  items: [],
+  totalPrice: 0,
+  count: 0,
+  add(name, price, amount = 1) {
+    this.items.push({name, price, amount});
 
-		this.increaseCount(amount);
-	},
-	increaseCount(num) {
-		this.count = this.items.reduce((acc, item) => acc + num, 0);
-	},
-	calculateItemPrice() {
-		return this.items.reduce((acc, item) => acc + item.price * item.amount, 0)
-	},
-	clear() {
-		this.items = [];
-		this.totalPrice = 0;
-		this.count = 0;
-	},
-	print() {
-		console.log(JSON.stringify(this.items));
-	},
-}
+    this.increaseCount(amount);
+  },
+  increaseCount(num) {
+    this.count = this.items.reduce((acc, item) => acc + num, 0);
+  },
+  calculateItemPrice() {
+    return this.items.reduce((acc, item) => acc + item.price * item.amount, 0);
+  },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+  print() {
+    console.log(JSON.stringify(this.items));
+  },
+};
 
 Object.defineProperty(cart, 'totalPrice', {
-	get() {
-		return this.calculateItemPrice();
-	}
+  get() {
+    return this.calculateItemPrice();
+  },
 });
 
 cart.add('Apples', 40);
@@ -36,4 +36,4 @@ cart.add('Bananas', 100);
 cart.add('Oranges', 50);
 
 cart.print();
-//cart.clear();
+// cart.clear();
